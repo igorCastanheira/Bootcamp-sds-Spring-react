adbeb939712540bf700aa990ff48213b64b0befc
Igor Castanheira <igor.castanheira.dev@gmail.com>
2021-05-07 15:57:17 -0300

    Bar Chart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,17 @@
+
+
+
 export type SaleSum = {
-    
-        sellerName: string,
-        sum: number
-    
+
+    sellerName: string,
+    sum: number
+
+}
+export type SaleSucces = {
+
+
+    sellerName: string;
+    visited: number;
+    deals: number;
+
 }
